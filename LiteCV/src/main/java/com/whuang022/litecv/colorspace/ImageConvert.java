172906f3ad52263c0741670cc0c9fd9703e7d365
whuang022 <whuang022@gmail.com>
2017-11-03 19:27:01 +0800

    color space add hsv

--- LiteCV/src/main/java/com/whuang022/litecv/colorspace/ImageConvert.java
+++ LiteCV/src/main/java/com/whuang022/litecv/colorspace/ImageConvert.java
@@ -20,6 +20,10 @@ public class ImageConvert
         {
             return convertGray(src);
         }
+        else  if(typ.equals("com.whuang022.litecv.colorspace.ImageHSV"))
+        {
+            return convertHSV(src);
+        }
         return null;
     }
     private Image convertGray(BufferedImage src)
@@ -48,7 +52,7 @@ public class ImageConvert
         gray.G= mix;
         return gray;
     }
-    private ImageRGB convertRGB(BufferedImage src)
+    private Image convertRGB(BufferedImage src)
     {
        int w1 = src.getWidth();
        int h1 = src.getHeight();
@@ -78,5 +82,77 @@ public class ImageConvert
        rgb.G=mixG;
        rgb.B=mixB;
        return rgb;
+    }
+    private Image convertHSV(BufferedImage src)
+    {
+       int w1 = src.getWidth();
+       int h1 = src.getHeight();
+       double [][] mixH= new double[h1][w1];
+       double [][] mixS= new double[h1][w1];
+       double [][] mixV= new double[h1][w1];
+       int valueR=0;
+       int valueG=0;
+       int valueB=0;
+       for (int i = 0; i < h1; i++)
+       {
+            for (int j = 0; j < w1; j++) 
+            {
+                valueR = src.getRGB(j, i); 
+                valueG = src.getRGB(j, i);
+                valueB = src.getRGB(j, i);
+                int R = ( valueR >> 16) & 0xff;
+                int G = ( valueG >> 8) & 0xff;
+                int B = ( valueB) & 0xff;
+                double H=0,S=0,V=0;
+                //
+                double max = R;
+                double min = R;
+                if (G > max) {
+                    max = G;
+                }
+                if (B> max) {
+                    max = B;
+                }
+
+                if (G < min) {
+                    min = G;
+                }
+                if (B < min) {
+                    min = B;
+                }
+                //
+                V=max;
+                double D=max-min;
+                if(R==G&&G==B)
+                {
+                    H=0;
+                }
+                else
+                {
+                    double theta=( (0.5*((R-G)+(R-B)))/Math.sqrt( (R-G)*(R-G)+(R-B)*(G-B)));
+                    H=Math.acos(theta);
+                }
+                if(B>G)
+                {
+                    H=2*Math.PI-H;
+                }
+                if(max>0)
+                {
+                    S=D/max;
+                }
+                else
+                {
+                    S=0;
+                }
+                mixH[i][j] = H;
+                mixS[i][j] = S;
+                mixV[i][j] = V;
+            }
+       }
+       ImageHSV hsv=new ImageHSV();
+       hsv.H=mixH;
+       hsv.S=mixS;
+       hsv.V=mixV;
+       return hsv;
     }
 }
